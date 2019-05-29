69a3c734c9eac81cc11e624b7835a8c12ac59860
ddolphin <ddolphin.loves.gc@gmail.com>
2019-05-29 11:09:26 +0900

    登録確認画面の追加

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -47,18 +47,18 @@ class UsersController extends AppController
      */
     public function add()
     {
+        $user = $this->Users->newEntity();
         if ($this->request->is('post')) {
-            if($this->request->getData()['User']['mode'] == "confirm"){
+            if($this->request->getData(['mode']) == "confirm"){
                 $this->render('confirm');
             } else {
-                $user = $this->Users->newEntity();
                 $user = $this->Users->patchEntity($user, $this->request->getData());
                 if ($this->Users->save($user)) {
                     $this->Flash->success(__('The user has been saved.'));
-
                     return $this->redirect(['action' => 'index']);
+                } else {
+                    $this->Flash->error(__('The user could not be saved. Please, try again.'));
                 }
-                $this->Flash->error(__('The user could not be saved. Please, try again.'));
             }
         }
         $this->set(compact('user'));
--- src/Template/Users/confirm.ctp
+++ src/Template/Users/confirm.ctp
@@ -3,11 +3,12 @@
 	<fieldset>
 		<legend><?php echo "入力内容をご確認ください"; ?></legend>
 		<dl>
-			<dt><?php echo __("Subject"); ?>&nbsp;</dt>
-			<dt><?php echo $this->Form->value('subject'); ?>&nbsp;</dt>
+			<dt><?php echo __("Email"); ?>&nbsp;</dt>
+			<dt><?php echo $this->Form->value('email'); ?>&nbsp;</dt>
+			<dt><?php echo __("Password"); ?>&nbsp;</dt>
+			<dt><?php echo $this->Form->value('password'); ?>&nbsp;</dt>
 		</dl>
-		<?php
-		echo $this->Form->hidden('subject', array('value' => $this->Form->value("subject")))."\n";?>
 	</fieldset>
-	<?php echo $this->Form->end(__('Finish')); ?>
+	<?= $this->Form->button(__('Submit')) ?>
+	<?php echo $this->Form->end(); ?>
 </div>
